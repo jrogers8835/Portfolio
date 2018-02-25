147fb90aec8a18d16d124fdca404050714e21674
Casey Rogers <casey_rogers@homedepot.com>
2018-02-24 20:02:58 -0500

    Attempted website

---
+++ server.js
@@ -0,0 +1,16 @@
+const express		= require('express');
+
+const app			= express();
+const port 			= 8080;
+
+app.listen(port, () => {
+	console.log('We are live on ' + port);
+});
+
+app.get('/', function (req, res) {
+	res.send('Hello World!');
+});
+
+app.get('/home', function (req, res) {
+	res.send(index.html);
+});
