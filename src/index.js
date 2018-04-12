377beb12b35aedee5b7fbbd4be0818357090d747
Casey <jrogers8835@gmail.com>
2018-04-11 22:37:26 -0400

    restructure fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Homepage from './components/Homepage.js';
+import Homepage from './components/Homepage/Homepage.js';
 
 ReactDOM.render(
   <Homepage />,
